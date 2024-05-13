4c4074aaba8c908bed1337eeeab016c084688913
Rahul Kumar Yadav <rahulkumaryadav2107@gmail.com>
2024-05-13 17:53:20 +0530

    Hello

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>Home components</h1>
+    <h1>{{text}}</h1>
+    <div v-html="tag"></div>
+    <div v-html="name"></div>
+  </template>
+  
+  <script>
+
+
+  export default {
+    name: 'HomeComponent',
+    data() {
+        return {
+            text: "Rahul Kumar Yadav",
+            tag:"<h1>HTML Biding</h1>",
+            name:"<h2>Divya</h2>"
+        }
+    }
+  }
+  </script>
+  
